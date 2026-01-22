cfb84b00bb5e59af1150539805c58078ae598a3b
Tanish0023 <tanish23aggarwal@gmail.com>
2026-01-22 22:01:09 +0530

    Completed section 8

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,10 @@
-from django.test import SimpleTestCase
+# from django.test import SimpleTestCase
 
-from app.calc import add
+# from app.calc import add
 
-class CalcTest(SimpleTestCase):
-  def test_numbers(self):
-    res = add(5,10)
-    
-    self.assertEqual(res, 15)
+
+# class CalcTest(SimpleTestCase):
+#     def test_numbers(self):
+#         res = add(5, 10)
+
+#         self.assertEqual(res, 15)
